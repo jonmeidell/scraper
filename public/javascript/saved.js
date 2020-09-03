5cdda9be7a6745fdbcfef56c33bca4cddfdbd66e
jonmeidell <jonmeidell@gmail.com>
2020-09-02 21:16:07 -0600

    initial work on saved page

---
+++ public/javascript/saved.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+    const articleContainer = $(".article-container");
+    $(document).on("click", ".btn.delete", deleteArticle);
+    $(".clear").on("click", clearSavedArticles);
+
+    function initPage() {
+        $.get("/api/headlines?saved=true").then(function (data) {
+            articleContainer.empty();
+            if (data && data.length) {
+                renderArticles(data);
+            } else {
+                renderEmpty();
+            }
+        });
+    };
+
+    function renderArticles(articles) {
+        var articleCards = [];
+        for (let i = 0; i < articles.length; i++) {
+            articleCards.push(createCard(articles[i]));
+        }
+        articleContainer.append(articleCards);
+    };
+
+    function createCard(article) {
+        const card = $("<div class='card'>");
+        const cardHeader = $("<div class='card-header'>").append(
+            $("<h3>").append(
+                $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
+                    .attr("href", article.url)
+                    .text(article.headline),
+                $("<a class='btn btn-danger delete'>Delete From Saved</a>"),
+            )
+        );
+
+        const cardBody = $("<div class='card-body'>").text(article.summary);
+
+        card.append(cardHeader, cardBody);
+        card.data("_id", article._id);
+        return card;
+    };
+
+    function renderEmpty() {
+        var emptyAlert = $(
+            [
+                "<div class='alert alert-warning text-center'>",
+                "<h4>Nothing here. It looks like you haven't any saved articles yet.</h4>",
+                "</div>",
+                "<div class='card'>",
+                "<div class='card-header text-center'>",
+                "<h3>Would You Like to Browse Available Articles?</h3>",
+                "</div>",
+                "<div class='card-body text-center'>",
+                "<h4><a href='/'>Browse Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join("")
+        );
+        articleContainer.append(emptyAlert);
+    };
+
+    function deleteArticle() {
+        const articleToDelete = $(this)
+            .parents(".card")
+            .data();
+        $(this)
+            .parents(".card")
+            .remove();
+        $.ajax({
+            method: "DELETE",
+            url: "/api/headlines/" + articleToDelete._id
+        }).then(function (data) {
+            if (data.ok) {
+                initPage();
+            }
+        });
+    }
+    function handleArticleNotes(event) {
+        const currentArticle = $(this)
+            .parents(".card")
+            .data();
+        $.get("/api/notes/" + currentArticle._id).then(function (data) {
+            const modalText = $("<div class='container-fluid text-center'>").append(
+                $("<h4>").text("Notes For Article: " + currentArticle._id),
+                $("<hr>"),
+                $("<ul class='list-group note-container'>"),
+                $("<textarea placeholder='New Note' rows='4' cols='60'>"),
+                $("<button class='btn btn-success save'>Save Note</button>")
+            );
+            bootbox.dialog({
+                message: modalText,
+                closeButton: true
+            });
+            const noteData = {
+                _id: currentArticle._id,
+                notes: data || []
+            };
+            $(".btn.save").data("article", noteData);
+            renderNotesList(noteData);
+        });
+    }
+
+    function handleNoteSave() {
+        const noteData;
+        const newNote = $(".bootbox-body textarea")
+            .val()
+            .trim();
+        if (newNote) {
+            noteData = { _headlineId: $(this).data("article")._id, noteText: newNote };
+            $.post("/api/notes", noteData).then(function () {
+                bootbox.hideAll();
+            });
+        }
+    }
+
+    function handleNoteDelete() {
+        const noteToDelete = $(this).data("_id");
+        $.ajax({
+            url: "/api/notes/" + noteToDelete,
+            method: "DELETE"
+        }).then(function () {
+            // When done, hide the modal
+            bootbox.hideAll();
+        });
+    }
+
+    function clearSavedArticles() {
+        $.get("api/clear")
+            .then(function () {
+                articleContainer.empty();
+                initPage();
+            });
+    }
+});
